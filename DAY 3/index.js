b84aa63b9542f51af9c91672ebd93d0dc261b3f7
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-02-17 01:53:34 -0300

    IMC

--- DAY 3/index.js
+++ DAY 3/index.js
@@ -1,1 +1,29 @@
-let a = "Wellyngthon Sanveira";
+let nome = "";
+let idade = 0;
+let altura = 0;
+let peso = 0;
+
+nome = prompt("SEU NOME");
+idade = prompt("SUA IDADE");
+altura = prompt("SUA ALTURA");
+peso = prompt("SEU PESO");
+
+var imc = peso / (altura * altura);
+
+var anoNascimento = 2023 - idade;
+
+var msg =
+  "Olá " +
+  nome +
+  ", você tem " +
+  idade +
+  ", nasceu em " +
+  anoNascimento +
+  ", tem " +
+  altura +
+  " de altura, pesa " +
+  peso +
+  " e seu IMC é " +
+  imc +
+  " Kg / m2 ";
+document.write(msg);
