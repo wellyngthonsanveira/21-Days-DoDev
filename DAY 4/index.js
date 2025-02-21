3a54c6f87a5284f57a0ebd325e076d8fcdd9f944
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-02-20 23:45:16 -0300

    day4

--- DAY 4/index.js
+++ DAY 4/index.js
@@ -1,5 +1,7 @@
 let condicao = false;
+let idade = 2 != "5";
 if (condicao == true) {
+  let scope = "DentroEscopo";
   document.write("Entrou no IF");
 } else {
   document.write("Entrou no Else");
