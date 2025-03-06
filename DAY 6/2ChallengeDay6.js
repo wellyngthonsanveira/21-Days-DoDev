d49d106038bc72b40e28bb6985269a929c1fb7e6
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-03-06 01:31:03 -0300

    day6

--- DAY 6/2ChallengeDay6.js
+++ DAY 6/2ChallengeDay6.js
@@ -3,17 +3,17 @@ let table = parseInt(prompt("Number of Table"));
 for (let i = 1; i <= 3; i++) {
   if (i == 1) {
     for (p = 0; p <= 10; p++) {
-      document.write(table + " x " + p + " = " + table * p + "<br>");
+      document.write(table + " x " + p + " = " + table * p + "<br><br>");
     }
   } else if (i == 2) {
     table += 1;
     for (s = 0; s <= 10; s++) {
-      document.write(table + " x " + s + " = " + table * s + "<br>");
+      document.write(table + " x " + s + " = " + table * s + "<br><br>");
     }
   } else {
     table += 1;
     for (t = 0; t <= 10; t++) {
-      document.write(table + " x " + t + " = " + table * t + "<br>");
+      document.write(table + " x " + t + " = " + table * t + "<br><br>");
     }
   }
 }
