85b990c5edcf5438a9589455d94f5f96a287a6b4
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-03-06 00:35:57 -0300

    DAY6

--- DAY 6/index.js
+++ DAY 6/index.js
@@ -1,5 +1,12 @@
-document.write("Hello");
-
-for (let contador = 0; contador < 5; contador++) {
-  document.write("o contador é " + contador);
+let op = parseInt(prompt("enter table"));
+document.write("table of " + op + " is: <br>");
+document.write("---------------- + -------------- <br>");
+for (let contador = 0; contador <= 10; contador++) {
+  document.write(op + " + " + contador + "=" + (op + contador) + "<br>");
+  if (contador == 10) {
+    document.write("---------------- X -------------- <br>");
+    for (let x = 0; x <= 10; x++) {
+      document.write(op + " x " + x + "=" + op * x + "<br>");
+    }
+  }
 }
