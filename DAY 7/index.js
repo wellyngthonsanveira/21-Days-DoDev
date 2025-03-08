5ba68fc5f1fe6349bd8d7bfcc7b5b76b8f81146d
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-03-08 17:46:16 -0300

    day7

--- DAY 7/index.js
+++ DAY 7/index.js
@@ -1,1 +1,9 @@
-let 
+let condition = true;
+while (condition) {
+  let Yourage = Number(prompt("Your age"));
+  if (Yourage < 18) {
+    document.write("You are a minor <br>");
+    condition = false;
+  }
+}
+document.write("outside the while");
