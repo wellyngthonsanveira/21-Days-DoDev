bf246710100500de7b2c4eb15f7db9ee69bbd21e
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2025-05-19 00:19:40 -0300

    RH

---
+++ DAY 9/challengeRH.js
@@ -0,0 +1,32 @@
+let YourName = prompt("Enter your name");
+let salary = parseInt(prompt("Enter your salary"));
+let age = prompt("Enter your age");
+
+alert(YourName + " : " + salary + " : " + age);
+let confirmation = true;
+
+while (confirmation) {
+  confirmation = prompt("y to yes and n to no");
+  if (confirmation == "y") {
+    confirmation = false;
+  } else {
+    YourName = prompt("Enter your name");
+    salary = prompt("Enter your salary");
+    age = prompt("Enter your age");
+    alert(YourName + " : " + salary + " : " + age);
+  }
+}
+let CurrentYear = 2025;
+let tax = 0.015;
+salary = salary + salary * tax;
+let IncreaseSalary = salary;
+console.log(IncreaseSalary);
+
+let hiredStatusNextYear = prompt("HIRED or NOTHIRED in the next year");
+
+while (hiredStatusNextYear == "h") {
+  CurrentYear = CurrentYear + 1;
+  tax *= 2;
+  console.log(CurrentYear + " : " + (salary + salary * tax));
+  hiredStatusNextYear = prompt("HIRED or NOTHIRED in the next year");
+}
